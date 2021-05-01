db7e8b359ca57d5e200a27bea6f03d7328bbb769
Bruno Alliguieri <alliguierisilva@hotmail.com>
2021-05-01 14:12:49 -0300

    HttpStatus given to annotation.

--- src/main/java/com/zerotospring/zerotospring/Exceptions/BadRequestException.java
+++ src/main/java/com/zerotospring/zerotospring/Exceptions/BadRequestException.java
@@ -1,8 +1,9 @@
 package com.zerotospring.zerotospring.Exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
   public BadRequestException(String message) {
     super(message);
